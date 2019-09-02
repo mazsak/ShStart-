b9fcddf0702e7a72198843f468864ac39ff23b43
Mateusz Samluk <mazsak97@gmail.com>
2019-09-02 13:02:22 +0200

    save person

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.IPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,8 +17,8 @@ public class HomeController {
     @Autowired
     IPersonService personService;
 
-    @RequestMapping({"/",""})
-    public String home(){
+    @RequestMapping({"/", ""})
+    public String home() {
         /*Person person = new Person();
         person.setName("Jan");
         person.setAge(15);
@@ -25,11 +27,23 @@ public class HomeController {
         personService.save(person);*/
         return "home";
     }
+
     @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public String  all(Model model) {
-       model.addAttribute("persons", personService.findAll());
-       return "all";
+    public String all(Model model) {
+        model.addAttribute("persons", personService.findAll());
+        return "all";
     }
 
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("person", new Person());
+        return "savePerson";
+    }
+
+    @PostMapping("/save")
+    public String save(Person person) {
+        personService.save(person);
+        return "index";
+    }
 
 }
